7af41e3ea365f8adad497e8b3e1fa6eae17708ff
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-02-07 22:31:28 +0800

    Added basic keplr connection

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-// javascript
+const chainId = "cosmoshub-4";
 
-console.log("Hello world");
+const connectBtn = document.getElementById('connect')
+let offlineSigner
+
+connectBtn.addEventListener('click', async () => {
+    console.log("connect button clicked");
+
+    if(!window.keplr) {
+        alert("Please install keplr extension")
+    }
+
+    try {
+        await keplr.enable(chainId);
+        offlineSigner = window.getOfflineSigner(chainId)
+        const accounts = await offlineSigner.getAccounts()
+        document.getElementById('address').textContent = 'Connected: ' + accounts[0].address
+        connectBtn.textContent = 'Connected'
+        connectBtn.disabled = true
+    } catch(err) {
+        console.error(err)
+    }
+})
